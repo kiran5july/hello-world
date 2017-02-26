faeb7789baf45811c711aac937ce941cbe540629
kiran5july <kiran_wp@yahoo.com>
2017-02-25 19:59:11 -0600

    More files added

--- src/main/java/com/kiran/DBConnExecAnySQL.java
+++ src/main/java/com/kiran/DBConnExecAnySQL.java
@@ -1,65 +1,73 @@
 package com.kiran;
 import java.sql.*;
 
+import com.kiran.mr.WordCountByKey;
+
 public class DBConnExecAnySQL {
 
 
   public static void main(String args[]) {
-   Connection conn=null;
-   Statement stmt=null;
-   ResultSet rs=null;
+	  System.out.println("args:" + args.length);
+	  if(args.length != 3){
+		  System.err.printf("KM USAGE ERROR: %s <userName> <password> <SQL>\n", DBConnExecAnySQL.class);
+			//ToolRunner.printGenericCommandUsage(System.err);
+		  System.exit(1);
+		  //return;
+	  }
+		  
+	  String sUser = args[0];
+	  String sPwd = args[1];
+	  String oSQL = args[2];
+	  
+	  Connection conn=null;
+	  Statement stmt=null;
+	  ResultSet rs=null;
 
-   if (args.length==0){
-     System.out.println("Usage: java ShowAnyData SQLSelectStatement");
-     System.out.println("For example: java ShowAnyData \"Select * from Employee\"");
-     System.exit(1);
-   }
 
-   try {
-	// Class.forName("org.apache.derby.jdbc.ClientDriver");
-     //conn = DriverManager.getConnection( "jdbc:derby://localhost:1527/Lesson22"); 
-	   Class.forName("com.mysql.jdbc.Driver");
-	    conn = DriverManager.getConnection( "jdbc:mysql://localhost:3306/retail_db","km","Kiran$5july@123");
-	    
-    // Create a Statement object
-    stmt = conn.createStatement(); 
+	  try {
+		  // Class.forName("org.apache.derby.jdbc.ClientDriver");
+		  //conn = DriverManager.getConnection( "jdbc:derby://localhost:1527/Lesson22"); 
+		  Class.forName("com.mysql.jdbc.Driver");
+		  conn = DriverManager.getConnection( "jdbc:mysql://localhost:3306/retail_db", sUser, sPwd);
 
-    // Execute SQL Select 
-    rs = stmt.executeQuery(args[0]);  
+		  // Create a Statement object
+		  stmt = conn.createStatement(); 
 
-    // Find out the number of columns, their names and display the data
-    ResultSetMetaData rsMeta = rs.getMetaData();
-    int colCount = rsMeta.getColumnCount();
+		  // Execute SQL Select 
+		  rs = stmt.executeQuery(oSQL);  
 
-     for (int i = 1; i <= colCount; i++)  {
-      System.out.print(rsMeta.getColumnName(i) + " "); 
-    }
-    System.out.println();
+		  // Find out the number of columns, their names and display the data
+		  ResultSetMetaData rsMeta = rs.getMetaData();
+		  int colCount = rsMeta.getColumnCount();
+
+		  for (int i = 1; i <= colCount; i++)  {
+			  System.out.print(rsMeta.getColumnName(i) + " "); 
+		  }
+		  System.out.println();
    
-    while (rs.next()){
-    	System.out.print("[");
-       for (int i = 1; i <= colCount; i++)  {
-         System.out.print(rs.getString(i) + " "); 
-       }
-       System.out.print("]\n");   // new line character
-    }
+		  while (rs.next()){
+			  System.out.print("[");
+			  for (int i = 1; i <= colCount; i++)  {
+				  System.out.print(rs.getString(i) + " "); 
+			  }
+			  System.out.print("]\n");   // new line character
+		  }
 
-   } catch( SQLException se ) {
-      System.out.println ("SQLError: " + se.getMessage ()
-           + " code: " + se.getErrorCode ());
+	  } catch( SQLException se ) {
+		  System.out.println ("SQLError: " + se.getMessage () + " code: " + se.getErrorCode ());
 
-   } catch( Exception e ) {
-      System.out.println(e.getMessage()); 
-      e.printStackTrace(); 
-   } finally{
-       // clean up the system resources
-       try{
-	   rs.close();     
-	   stmt.close(); 
-	   conn.close();  
-       } catch(Exception e){
-           e.printStackTrace();
-       } 
-   }
- }
+	  } catch( Exception e ) {
+		  System.out.println(e.getMessage()); 
+		  e.printStackTrace(); 
+	  } finally{
+		  // clean up the system resources
+		  try{
+			  rs.close();     
+			  stmt.close(); 
+			  conn.close();  
+		  } catch(Exception e){
+			  e.printStackTrace();
+		  } 
+	  }
+  }
 }
